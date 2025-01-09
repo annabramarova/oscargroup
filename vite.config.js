d96561ba38dd041048133f16b4e94555e4dedabf
Anna Bramarova <annabramarova@gmail.com>
2025-01-09 15:44:37 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -8,20 +8,17 @@ export default defineConfig({
   base: './',
   build: {
     rollupOptions: {
-      input: [...glob.sync('./src/*.html'), ...glob.sync('./src/tr/*.html')],
+      input: [
+        ...glob.sync('./src/*.html'), 
+        ...glob.sync('./src/tr/*.html'),
+      ],
     },
     outDir: '../dist',
   },
   server: {
     open: true,
     host: '0.0.0.0',
-    proxy: {
-      '/api': {
-        target: 'https://api.oskargroup.live', 
-        changeOrigin: true, 
-        rewrite: path => path.replace(/^\/api/, ''), 
-      },
-    },
   },
+
   plugins: [injectHTML(), FullReload(['./src/**/*'])],
 });
