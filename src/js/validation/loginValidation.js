f556989f98a5223f2ea1fa4ef4acac75dbf935ab
Anna Bramarova <annabramarova@gmail.com>
2025-01-09 09:32:03 +0200

    updater

--- src/js/validation/loginValidation.js
+++ src/js/validation/loginValidation.js
@@ -1,18 +1,16 @@
 import Bouncer from 'formbouncerjs';
 import { BouncerConfig } from './bouncerConfig';
+import { handleLoginSubmit } from '../ajax';
 
-// Получаем форму
+// Отримуємо форму
 const loginForm = document.querySelector('.js-login-form');
 
-// Инициализируем Bouncer, передавая сам элемент формы
 const loginFormValidation = new Bouncer('.js-login-form', BouncerConfig);
 
 loginForm.addEventListener('submit', e => {
-  
-loginForm.addEventListener('submit', e => {
   if (!loginFormValidation.validate()) {
     e.preventDefault();
+  } else {
+    handleLoginSubmit(e);
   }
-});
-
 });
