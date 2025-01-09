60dbbb6d765bfe7bb7c364d0d8d75013ad038e02
Anna Bramarova <annabramarova@gmail.com>
2025-01-09 13:36:03 +0200

    fix

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,38 +1,29 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const url = 'oskargroup.live';
+const url = "oscargroup.live"
 
-export async function handleLoginSubmit(event) {
-  event.preventDefault();
-
-  const email = document.getElementById('email-login').value;
-  const password = document.getElementById('password-login').value;
-
+export async function handleLoginSubmit(email, password) {
   try {
     const response = await fetch(`https://api.${url}/login`, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Type': 'application/json',
       },
-      body: new URLSearchParams({
-        email,
-        password,
+      body: JSON.stringify({
+        email: email,
+        password: password,
       }),
     });
 
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-
     const resp = await response.json();
+
     if (resp.success) {
       Notify.success('Login successful! Redirecting...');
-      window.location.href = resp.autologin;
+      window.location.href = resp.autologin; // Переход по ссылке
     } else {
-      Notify.failure(resp.message); 
+      Notify.failure(resp.message);
     }
   } catch (error) {
-    console.error('Error during login:', error);
     Notify.failure(
       'An error occurred while trying to log in. Please try again later.'
     );
--- src/js/validation/loginValidation.js
+++ src/js/validation/loginValidation.js
@@ -3,10 +3,17 @@ import { BouncerConfig } from './bouncerConfig';
 import { handleLoginSubmit } from '../fetch';
 
 const loginForm = document.querySelector('.js-login-form');
-new Bouncer('.js-login-form', BouncerConfig);
 
-loginForm.addEventListener('bouncerFormValid', async e => {
-  e.preventDefault(); 
-  
-  await handleLoginSubmit(e); 
+  console.log(loginForm);
+const bouncer = new Bouncer('.js-login-form', BouncerConfig);
+
+loginForm.addEventListener('submit', async e => {
+  e.preventDefault();
+
+    loginForm.addEventListener('bouncerFormValid', async () => {
+      const email = loginForm.querySelector('#email-login').value; 
+      const password = loginForm.querySelector('#password-login').value;  
+
+      await handleLoginSubmit(email, password);
+    });
 });
